60579899ceb815c677c8ea2869eecf92d59c9619
Thomas <tperry@cern.ch>
2015-02-24 10:05:06 -0600

    don't transfer executable

--- template_runCondor.sh
+++ template_runCondor.sh
@@ -4,7 +4,7 @@ cat > submit <<EOF
 Executable = SUBMITDIR/SAMPLE_runCallAnalyzer.sh
 GetEnv = True
 should_transfer_files = yes
-transfer_executable = yes
+transfer_executable = no
 when_to_transfer_output = on_exit
 Requirements = TARGET.Arch == "x86_64" && HasAFS_OSG && TARGET.UWCMS_CVMFS_Exists && TARGET.CMS_CVMFS_Exists
 transfer_input_files = 

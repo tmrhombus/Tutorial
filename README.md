d29555fe2477f9efa4e408d8ea0dea333ca107ec
Thomas <tperry@cern.ch>
2015-02-24 10:03:13 -0600

    for setting environment variables

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Tutorial
 MakeClass Analyzer and condor_submit Submission 
+
+To check out
+
+```
+scram pro -n Tutorial_CMSSW_5_3_24 CMSSW CMSSW_5_3_24
+cd Tutorial_CMSSW_5_3_24/src
+cmsenv
+git clone https://github.com/tmrhombus/Tutorial.git
+cd Tutorial
+source setup.sh
+```
+
+You need to ``source setup.sh`` each time you log on
---
+++ setup.sh
@@ -0,0 +1,8 @@
+export runname="Shmootorial"
+# where the analyzer is kept
+export alzr_dir="$(pwd)"
+# where we eventually accesss the files from
+export base_in_dir="root://cmsxrootd.hep.wisc.edu//store/user/tperry/Tutorial/"
+export hdfs_in_dir="/hdfs/store/user/tperry/Tutorial/" # needed for find
+# where we'll we submit from
+export base_submit_dir="/nfs_scratch/$USER/$runname/"

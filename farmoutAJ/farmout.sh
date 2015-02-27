7c049f51f0cb9501d543d86ec118dc3be4316451
Thomas <tperry@cern.ch>
2015-02-27 17:07:04 -0600

    generalize filepaths

--- farmoutAJ/farmout.sh
+++ farmoutAJ/farmout.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-farmoutAnalysisJobs --infer-cmssw-path --fwklite --input-file-list=list.txt --extra-inputs=/afs/hep.wisc.edu/cms/tperry/Tutorial_CMSSW_5_3_24/src/Tutorial/farmouttests/tomsAnalyzer.C,/afs/hep.wisc.edu/cms/tperry/Tutorial_CMSSW_5_3_24/src/Tutorial/farmouttests/tomsAnalyzer.h DumbiaInfinity callAnalyzer.cc
+farmoutAnalysisJobs --infer-cmssw-path --fwklite --input-file-list=list.txt --extra-inputs="${CMSSW_BASE}/src/Tutorial/farmoutAJ/tomsAnalyzer.C,${CMSSW_BASE}/src/Tutorial/farmoutAJ/tomsAnalyzer.h" ${runname} farmoutAnalyzer.cc
 
--- template_runCallAnalyzer.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-root -l -b -q  SAMPLE_callAnalyzer.cc

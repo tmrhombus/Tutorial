f69c75c16babda47c25115f698f1996f521e4f28
Thomas <tperry@cern.ch>
2015-02-27 17:23:37 -0600

    use same analyzer for condor_submit and FAJ

--- setup.sh
+++ setup.sh
@@ -3,7 +3,9 @@ export runname="Shmootorial"
 export xrootd="root://cmsxrootd.hep.wisc.edu//store/user/${USER}"
 export hdfs="/hdfs/store/user/${USER}" 
 export nfs="/nfs_scratch/${USER}"
-    
+ 
+export alzr_dir="$(pwd)"
+
 # where we eventually accesss the files from
 export base_in_dir="root://cmsxrootd.hep.wisc.edu//store/user/tperry/Tutorial/"
 export hdfs_in_dir="/hdfs/store/user/tperry/Tutorial/" # needed for find

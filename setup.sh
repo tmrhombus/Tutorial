c0c673223590808e30d873cadc6251e68c4f0093
Thomas <tperry@cern.ch>
2015-02-27 17:08:09 -0600

    clean up environment variables

--- setup.sh
+++ setup.sh
@@ -1,8 +1,12 @@
+cmsenv
 export runname="Shmootorial"
-# where the analyzer is kept
-export alzr_dir="$(pwd)"
+export xrootd="root://cmsxrootd.hep.wisc.edu//store/user/${USER}"
+export hdfs="/hdfs/store/user/${USER}" 
+export nfs="/nfs_scratch/${USER}"
+    
 # where we eventually accesss the files from
 export base_in_dir="root://cmsxrootd.hep.wisc.edu//store/user/tperry/Tutorial/"
 export hdfs_in_dir="/hdfs/store/user/tperry/Tutorial/" # needed for find
-# where we'll we submit from
-export base_submit_dir="/nfs_scratch/$USER/$runname/"
+# where we'll we submit from with condor_submit
+export base_submit_dir="/nfs_scratch/${USER}/${runname}/"
+
